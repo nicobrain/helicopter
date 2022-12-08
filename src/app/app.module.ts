5fbad1d50a76c1723cc14f2445cc52cc0b8c023a
nicobrain <nicobrain1@gmail.com>
2022-12-08 17:19:04 +0200

    1) First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HelicoptersComponent } from './helicopters/helicopters.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    HelicoptersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
